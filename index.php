775cdc5a9ae4282d4f7a00683865d9776eae9c5a
SpyDeXName <SpyDeX@users.noreply.github.com>
2017-11-09 19:28:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ function CheckProfileFits($payload, $profileTrigger, $hash)
     $flag = true;
     if ('Master-KEY' == $k)     $flag = $verify === hash_hmac($algo, $payloadRaw, $v); // Doesn't work. at all :(
     if ('branch' == $k)         $flag = 'refs/heads/' .$v ===  $payload['ref'];
-    if ('repo_full_name' == $k) $flag = $v === $payload['repository']['full_name'];    
+    if ('repo_full_name' == $k) $flag = $v === $payload['repository']['full_name'];
     if ('authors' == $k)        $flag = in_array($payload['head_commit']['author']['username'], $v);
     
     if (!$flag) $failOn[] = $k;
@@ -36,10 +36,10 @@ function CheckProfileFits($payload, $profileTrigger, $hash)
 foreach ($SynoGitSync_Profile as $profName => $prof)
 {
   $checkRes = CheckProfileFits($payload, $prof['On'], $PushKey);
-  if (!$checkRes)
+  if (!is_array($checkRes))
   {
     echo 'profile '.$profName.' is skipped by ['.$checkRes.'].'."\n";
-    continue;    
+    continue;
   }
   if (is_array($checkRes) && (count($checkRes) > 0))
   {
